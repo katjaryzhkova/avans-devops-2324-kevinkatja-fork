4c9829766aea432636884fe5611a81b71c5295a1
katjaryzhkova <e.ryzhkova@student.avans.nl>
2024-02-06 15:44:37 +0100

    week 1

---
+++ week 1/services/database.js
@@ -0,0 +1,9 @@
+const { MongoClient } = require("mongodb");
+const uri = process.env.MONGO_URL;
+const client = new MongoClient(uri);
+const db = client.db(process.env.DB_NAME);
+
+module.exports = {
+    db: db,
+    client: client
+};
