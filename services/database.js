4140e4476354e1bfa486ffb29fa15ee324adb622
katjaryzhkova <e.ryzhkova@student.avans.nl>
2024-04-06 17:14:37 +0200

    fix: minor changes

--- services/database.js
+++ services/database.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 const uri = process.env.MONGO_URL;
 const client = new MongoClient(uri);
-const db = client.db(process.env.DB_NAME);
+const db = client.db(process.env.DB_NAME || "devops");
 
 module.exports = {
     db: db,
